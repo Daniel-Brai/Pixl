14d15b2a5ab6c26d87dbbd4292b958425fc4bbd8
Daniel Brai <braidaniel2001@gmail.com>
2023-07-27 16:30:33 +0100

    dev(app): Added image loading as textures using paths

--- src/main.zig
+++ src/main.zig
@@ -3,12 +3,42 @@ const c = @cImport({
     @cInclude("raylib.h");
 });
 
-pub fn main() void {
+const Texture2DArrayList = std.ArrayList(c.Texture2D);
+
+pub fn main() error{OutOfMemory}!void {
+    c.SetConfigFlags(c.FLAG_WINDOW_RESIZABLE | c.FLAG_VSYNC_HINT);
     c.InitWindow(750, 500, "Pixl");
     defer c.CloseWindow();
 
+    var arena = std.heap.ArenaAllocator.init(std.heap.c_allocator);
+    defer arena.deinit();
+
+    var allocator = arena.allocator();
+
+    var textures = Texture2DArrayList.init(allocator);
+    defer {
+        for (textures.items) |texture| {
+            c.UnloadTexture(texture);
+        }
+        textures.deinit();
+    }
+
     while (!c.WindowShouldClose()) {
-        c.BeginDrawing();
-        defer c.EndDrawing();
+        if (c.IsFileDropped()) {
+            const dropped_files = c.LoadDroppedFiles();
+            defer c.UnloadDroppedFiles(dropped_files);
+
+            const dropped_files_slice = dropped_files.paths[0..dropped_files.count];
+            for (dropped_files_slice) |dropped_file_path| {
+                const texture = c.LoadTexture(dropped_file_path);
+                if (texture.id == 0) continue;
+                try textures.append(texture);
+            }
+        }
+
+        {
+            c.BeginDrawing();
+            defer c.EndDrawing();
+        }
     }
 }
