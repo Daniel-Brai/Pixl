80f311ad955196661bcc96c40b07504979b56ab1
Daniel Brai <braidaniel2001@gmail.com>
2023-07-27 10:51:31 +0100

    dev(app): Added raylib dependency

--- build.zig
+++ build.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const raylib_build = @import("libs/raylib/src/build.zig");
 
 // Although this function looks imperative, note that its job is to
 // declaratively construct a build graph that will be executed by an external
@@ -15,6 +16,11 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
+    const raylib = raylib_build.addRaylib(b, target, optimize, .{
+        .raygui = true,
+        .platform_drm = true,
+    });
+
     const exe = b.addExecutable(.{
         .name = "pixl",
         // In this case the main source file is merely a path, however, in more
@@ -24,6 +30,8 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
+    exe.linkLibrary(raylib);
+    exe.addIncludePath("libs/raylib/src");
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
     // step when running `zig build`).
@@ -51,20 +59,4 @@ pub fn build(b: *std.Build) void {
     // This will evaluate the `run` step rather than the default, which is "install".
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
-
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
-    const unit_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_unit_tests = b.addRunArtifact(unit_tests);
-
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_unit_tests.step);
 }
